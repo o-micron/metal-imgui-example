f1a4a033cae13e7fbb33c892168a09fd2d5b5d33
o-micron <omarsheriffathy@hotmail.com>
2022-09-06 14:57:39 +0200

    Update imgui branch

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -3,6 +3,10 @@
 git clone https://github.com/o-micron/imgui.git
 git clone --depth 1 --single-branch -b release-2.0.22 https://github.com/libsdl-org/SDL.git
 
+cd imgui
+git checkout MetalCppAutoReleasePool
+cd ../
+
 cd metal-cpp-beta
 mkdir build
 cd build
