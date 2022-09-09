98b284a617da8ef8f6f1684adda9ffd72d27f89c
o-micron <omarsheriffathy@hotmail.com>
2022-09-09 03:33:07 +0200

    Fix shadow variable

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,27 @@
 int
 main(int, char**)
 {
-    NS::AutoreleasePool* pool = NS::AutoreleasePool::alloc()->init();
+    NS::AutoreleasePool* mainPool = NS::AutoreleasePool::alloc()->init();
     renderer::mtl::windowing::SdlCoreData   sdlCoreData   = {};
     renderer::mtl::rendering::MetalCoreData metalCoreData = {};
     if (renderer::mtl::windowing::init(sdlCoreData, metalCoreData)) {
         if (renderer::mtl::rendering::init(metalCoreData, sdlCoreData)) {
             while (true) {
-                NS::AutoreleasePool* pool = NS::AutoreleasePool::alloc()->init();
+                NS::AutoreleasePool* loopPool = NS::AutoreleasePool::alloc()->init();
                 if (renderer::mtl::windowing::poll(sdlCoreData, metalCoreData)) {
                     if(renderer::mtl::windowing::update(sdlCoreData, metalCoreData)) {
                         renderer::mtl::rendering::render(metalCoreData);
                     }
                 } else {
-                    pool->release();
+                    loopPool->release();
                     break;
                 }
-                pool->release();
+                loopPool->release();
             }
             renderer::mtl::rendering::deInit(metalCoreData);
         }
         renderer::mtl::windowing::deInit(sdlCoreData);
     }
-    pool->release();
+    mainPool->release();
     return 0;
 }
